9d6c91e74ee9bffefec202df2f776b97f87fce4f
tyszw <ttyyszw@gmail.com>
2021-11-29 18:26:12 +0900

    feat: add Caco-2 model

---
+++ ADMET/ChEMBL/Absorption/Caco-2/README.md
@@ -0,0 +1,37 @@
+# Caco-2 permeability
+
+## Task
+
+- Regression
+
+- Given a Morgan fingerprint(r=2, 2048 dim), predict the rate of apical-to-basal transport in Caco-2 cell.
+
+## Dataset
+
+- Data size: 2489
+
+<div align="left">
+    <img src="img/data_distribution.png" width="400">
+</div>
+
+## Model
+
+- LightGBM regressor
+
+- Hyperparameters were optimized in 5-folds cross-validation with Optuna.
+
+- To train the model, run `train.py`.
+    - Example usage
+        ```bash
+        python train.py -o lgb_caco-2
+        ```
+
+## Accuracy
+
+|Corr Coef|R2|MAE|MSE|RMSE|
+|:----:|:----:|:----:|:----:|:----:|
+|0.70|0.49|0.40|0.30|0.30|
+
+<div align="left">
+      <img src="img/scatter_plot.png" width="400">
+</div>
---
+++ ADMET/ChEMBL/Absorption/Caco-2/infer.py
@@ -0,0 +1,59 @@
+from math import isinf
+import os
+import pickle
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+
+def make_scatter_plot(y_true, y_pred):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.scatter(y_pred, y_true, marker='o', s=25, c='dimgray', alpha=0.2)
+    ax.set_xlabel('Predicted value', fontsize=20, labelpad=20, weight='bold')
+    ax.set_ylabel('Experimental value', fontsize=20, labelpad=20, weight='bold')
+    ax.xaxis.set_tick_params(direction='out', labelsize=15, width=3, pad=10)
+    ax.yaxis.set_tick_params(direction='out', labelsize=15, width=3, pad=10)
+    ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+    ax.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(1))
+    ax.set_aspect('equal')
+    ax.grid()
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(2)
+    return fig
+
+
+def main():
+
+    # Data preparation
+    suppl = Chem.SDMolSupplier('./data/permeability_AB_ChEMBL.sdf')
+    mols = []
+    values = []
+    for mol in suppl:
+        value = float(mol.GetProp('Permeability'))
+        if isinf(value):
+            continue
+        mols.append(mol)
+        values.append(value)
+    fps = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048) for mol in mols]
+    X = np.array(fps)
+    y = np.array(values)
+
+    # Load model
+    with open('./model/lgb_caco-2.pkl', mode='rb') as f:
+        model = pickle.load(f)
+
+    # Predict
+    y_pred = model.predict(X)
+
+    fig = make_scatter_plot(y, y_pred)
+    output_path = 'img/'
+    os.makedirs(output_path, exist_ok=True)
+    image_output_path = os.path.join(output_path, 'scatter_plot.png')
+    fig.savefig(image_output_path, dpi=300, bbox_inches='tight', pad_inches=0.05)
+
+
+if __name__ == '__main__':
+    main()
