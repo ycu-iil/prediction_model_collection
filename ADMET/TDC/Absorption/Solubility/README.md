d874535718fafa64471d3b4271f81efbf926d06a
tyszw <ttyyszw@gmail.com>
2021-11-30 11:06:31 +0900

    feat: add Solubility model

---
+++ ADMET/TDC/Absorption/Solubility/README.md
@@ -0,0 +1,25 @@
+# Solubility, AqSolDB
+
+## Description
+
+>`Dataset Description`: Aqeuous solubility measures a drug's ability to dissolve in water. Poor water solubility could lead to slow drug absorptions, inadequate bioavailablity and even induce toxicity. More than 40% of new chemical entities are not soluble.
+>`Task Description`: Regression. Given a drug SMILES string, predict the activity of solubility.lipophilicity.
+>`Dataset Statistics`: 9,982 drugs.
+
+## LightGBM model performance
+
+|Corr Coef|R2|MAE|MSE|RMSE|
+|:----:|:----:|:----:|:----:|:----:|
+|0.85|0.72|0.88|1.53|1.23|
+
+<div align="center">
+    <img src="./img/scatter_plot.png" width="400" height="400">
+</div>
+
+## References
+
+[1] Sorkun, M.C., Khetan, A. & Er, S. AqSolDB, a curated reference set of aqueous solubility and 2D descriptors for a diverse set of compounds. Sci Data 6, 143 (2019).
+
+## Dataset License
+
+CC BY 4.0.
---
+++ ADMET/TDC/Absorption/Solubility/infer.py
@@ -0,0 +1,57 @@
+import os
+import pickle
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from tdc.single_pred import ADME
+
+
+def make_scatter_plot(y_true, y_pred):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.scatter(y_pred, y_true, marker='o', s=25, c='dimgray', alpha=0.2)
+    ax.set_xlabel('Predicted value', fontsize=20, labelpad=20, weight='bold')
+    ax.set_ylabel('Experimental value', fontsize=20, labelpad=20, weight='bold')
+    ax.xaxis.set_tick_params(direction='out', labelsize=15, width=3, pad=10)
+    ax.yaxis.set_tick_params(direction='out', labelsize=15, width=3, pad=10)
+    ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(2))
+    ax.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(2))
+    ax.set_aspect('equal')
+    ax.grid()
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(2)
+    return fig
+
+
+def smiles_to_morganfp(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    return AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)
+
+
+def main():
+
+    # Data preparation
+    data = ADME(name='Solubility_AqSolDB')
+    df = data.get_data(format='df')
+    df['MorganFP'] = df['Drug'].apply(lambda x: smiles_to_morganfp(x))
+    X = np.array(df['MorganFP'].tolist())
+    y = np.array(df['Y'])
+
+    # Load model
+    with open('./model/lgb_sol.pkl', mode='rb') as f:
+        model = pickle.load(f)
+
+    # Predict
+    y_pred = model.predict(X)
+
+    fig = make_scatter_plot(y, y_pred)
+    output_path = 'img/'
+    os.makedirs(output_path, exist_ok=True)
+    image_output_path = os.path.join(output_path, 'scatter_plot.png')
+    fig.savefig(image_output_path, dpi=300, bbox_inches='tight', pad_inches=0.05)
+
+
+if __name__ == '__main__':
+    main()
